208522a1121aab0fc45c2d010f46a1e8e6af4cfc
hi <hi@DESKTOP-1IP3BV6>
2025-09-16 21:55:41 +0530

    interest calculate

---
+++ Interest_calculate/src/com/wipro/bank/exception/BankValidationException.java
@@ -0,0 +1,8 @@
+package com.wipro.bank.exception;
+
+public class BankValidationException extends Exception {
+    @Override
+    public String toString() {
+        return "Invalid data"; 
+    }
+}
