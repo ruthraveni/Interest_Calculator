a240f907e970991b8efe1891d60766f05f3e8b42
ruthraveni <m.ruthraveni@gmail.com>
2025-09-18 14:06:10 +0530

    Update MainClass.java

--- Interest_calculate/src/com/wipro/bank/main/MainClass.java
+++ Interest_calculate/src/com/wipro/bank/main/MainClass.java
@@ -7,20 +7,26 @@ public class MainClass {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter Tenure (in years): ");
-        int tenure = sc.nextInt();
+       
+        System.out.print("Enter number of test cases: ");
+        int t = sc.nextInt();
+        for (int i = 1; i <= t; i++) {
+            System.out.println("\n Test Case " + i +  " ");
+            System.out.print("Enter Tenure (in years): ");
+            int tenure = sc.nextInt();
 
-        System.out.print("Enter Principal: ");
-        float principal = sc.nextFloat();
+            System.out.print("Enter Principal: ");
+            float principal = sc.nextFloat();
 
-        System.out.print("Enter Age: ");
-        int age = sc.nextInt();
+            System.out.print("Enter Age: ");
+            int age = sc.nextInt();
 
-        System.out.print("Enter Gender: ");
-        String gender = sc.next();
+            System.out.print("Enter Gender: ");
+            String gender = sc.next();
 
-        BankService service = new BankService();
-        service.calculate(principal, tenure, age, gender);
+            BankService service = new BankService();
+            service.calculate(principal, tenure, age, gender);
+        }
 
         sc.close();
     }
